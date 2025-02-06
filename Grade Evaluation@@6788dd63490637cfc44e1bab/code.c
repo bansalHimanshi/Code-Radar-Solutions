7e28782364921dfc90098c4fd7632d8f5c76f860
bansalHimanshi <himanshi281209@gmail.com>
2025-02-06 09:17:04 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -7,4 +7,21 @@ int main()
     {
         printf("Excellent");
     }
+    else if (a==B)
+    {
+        printf("Good");
+    }
+    else if (a==C)
+    {
+        printf("Average");
+    }
+    else if (a==D)
+    {
+        printf("Below Average");
+    }
+    else if (a==F)
+    {
+        printf("Fail");
+    }
+    
 }
