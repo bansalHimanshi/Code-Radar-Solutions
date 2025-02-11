411309f500a58287173d45ad964b1e538d16af77
bansalHimanshi <himanshi281209@gmail.com>
2025-02-11 10:53:03 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -41,7 +41,10 @@ switch (d)
     else if(c=='/'){
         if(b!=0){
         printf("%d",a/b);
-    }
+      }
+        else {
+        printf("error");
+      }
     }
     else{
         printf("error");
